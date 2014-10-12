90770d014fc7d3d25ec984080eb3c00fc290156e
forflo <forflo@users.noreply.github.com>
2014-10-12 15:34:56 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,35 +7,53 @@
 #		-> svn/
 #		-> mercurial/
 ##
-setup_createDirStruct(){
+shellyMeta_createDirStruct(){
 	cd
 	
-	clog 2 "[setup_createDirStruct()]" checking folder structure.
+	clog 2 "[shellyMeta_createDirStruct()]" checking folder structure.
 	if [ -d ~/repos ]; then
-		clog 2 "[setup_createDirStruct()]" The folder ~/repos already exists.
-		clog 2 "[setup_createDirStruct()]" Deleting ~/repos.
+		clog 2 "[shellyMeta_createDirStruct()]" The folder ~/repos already exists.
+		clog 2 "[shellyMeta_createDirStruct()]" Deleting ~/repos.
 	
 		rm -rf ~/repos || {
-			clog 1 "[setup_createDirStruct()]" error "while" deleting!
+			clog 1 "[shellyMeta_createDirStruct()]" error "while" deleting!
 			return 1
 		}
 	fi
 
-	clog 2 "[setup_createDirStruct()]" creating folder structure.
-	mkdir ~/repos 2> /dev/null || echo "[setup_createDirStruct()]" Could not create folder repos
-	mkdir ~/repos/git 2> /dev/null || echo "[setup_createDirStruct()]" Could not create folder git
-	mkdir ~/repos/svn || echo "[setup_createDirStruct()]" Could not create foler svn
-	mkdir ~/repos/mercurial || echo "[setup_createDirStruct()]" Could not create folder mercurial
-	clog 2 "[setup_createDirStruct()]" folder structure has been created.
+	clog 2 "[shellyMeta_createDirStruct()]" creating folder structure.
+	mkdir ~/repos 2> /dev/null || echo "[shellyMeta_createDirStruct()]" Could not create folder repos
+	mkdir ~/repos/git 2> /dev/null || echo "[shellyMeta_createDirStruct()]" Could not create folder git
+	mkdir ~/repos/svn || echo "[shellyMeta_createDirStruct()]" Could not create foler svn
+	mkdir ~/repos/mercurial || echo "[shellyMeta_createDirStruct()]" Could not create folder mercurial
+	clog 2 "[shellyMeta_createDirStruct()]" folder structure has been created.
 	
 	return 0
 }
 
-setup_dlRepos(){
+shellyMeta_runSubShelly(){
+	local oldpwd=$PWD
+	clog 2 "[shellyMeta_runSubShelly()]" Will now run subshellies.
+	
+	cd ${REPOS_PATH}/git/
+	for i in *; do
+		clog 2 "[shellyMeta_runSubShelly()]" Starting init.sh of $i.
+		cd $i
+		. init.sh || {
+			clog 1 "[shellyMeta_runSubShelly()]" init.sh of $i failed!
+			return 1
+		}
+		cd ..
+	done
+	
+	return 0
+}
+
+shellyMeta_dlRepos(){
 	cd ${REPOS_PATH}/git/
 	for i in ${SHELLY_SUBS[*]}; do
 		git clone "$i" || {
-			clog 1 "[setup_dlRepos()]" Could not clone Repo ${i:0:20} ...
+			clog 1 "[shellyMeta_dlRepos()]" Could not clone Repo ${i:0:20} ...
 			return 1
 		}
 	done
@@ -43,16 +61,16 @@ setup_dlRepos(){
 	return 0
 }
 
-setup_loadFiles(){
+shellyMeta_loadFiles(){
 	local meta_links=(
     		"https://raw.githubusercontent.com/forflo/shellyMeta/master/setup_conf.sh"
     		"https://raw.githubusercontent.com/forflo/shellyMeta/master/setup_lib.sh"
 	)
 	
-	echo loading additional files
+	echo "[shellyMeta_loadFiles()]" loading additional files
 	for i in ${meta_links[*]}; do
 		eval "$(curl ${i})" || {
-			echo Could not evaluate code for ${i:0:30} ... .
+			echo "[shellyMeta_loadFiles()]" Could not evaluate code for ${i:0:30} ... .
 			return 1
 		}
 	done
@@ -60,32 +78,32 @@ setup_loadFiles(){
 	return 0
 }
 
-setup(){
+shellyMeta_init(){
 	setup_loadFiles || {
 		echo Could not get library files
 		exit 1
 	}
 	
-	clog 2 "setup_init()" installing shelly subsystems.
+	clog 2 "[shellyMeta_init()]" installing shelly subsystems.
 	setup_createDirStruct || {
-		clog 1 "setup_init()" Could not setup the directory structure!
+		clog 1 "[shellyMeta_init()]" Could not setup the directory structure!
 		exit 1
 	}
 	
 	setup_dlRepos || {
-		clog 1 "setup_init()" Could not download other shelly repos
+		clog 1 "[shellyMeta_init()]" Could not download other shelly repos!
 		exit 1
 	}
 	
-	start_repoInits(){
-		:
-		
+	shellyMeta_runSubShelly || {
+		clog 1 "[shellyMeta_init()]" Could not run all shelly repo hooks!
+		exit 1
 	}
 	
 	exit 0
 }
 
 echo Will delete everything in ~/repo
-echo continue in 5 seconds
+echo continue in 5 seconds!
 sleep 5
-setup
+shellyMeta_init
