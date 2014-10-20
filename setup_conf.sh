01a3fe5c7d09128de9dce820da810ca419120d9a
forflo <forflo@users.noreply.github.com>
2014-10-20 21:47:34 +0200

    Update setup_conf.sh

--- setup_conf.sh
+++ setup_conf.sh
@@ -1,8 +1,3 @@
-SHELLY_SUBS=(
-    "https://github.com/forflo/shellyCode.git"
-    "https://github.com/forflo/shellyRepo.git"
-    "https://github.com/forflo/shellyConfig.git"
-    "https://github.com/forflo/shellyEditor.git"
-)
+SHELLY_SUBS=(shelly{Code,Repo,Editor,Config})
 
 REPOS_PATH="$HOME/repos"
